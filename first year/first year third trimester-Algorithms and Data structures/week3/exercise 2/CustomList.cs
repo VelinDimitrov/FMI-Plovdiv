da786a145d6f08215d3a8c17a31c707413bb03f2
VelinDimitrov <bgrullerbg@abv.bg>
2018-04-19 16:41:53 +0300

    Adding week 3

---
+++ first year/first year third trimester-Algorithms and Data structures/week3/exercise 2/CustomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_2
+{
+    class CustomList<T>
+    {
+        private T[] container;
+        private int counter;
+        
+        public CustomList()
+        {
+            this.container = new T[4];
+        }
+    }
+}
---
+++ first year/first year third trimester-OOP2/week4/exercise 1/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_1
+{
+    class Person
+    {
+        string name;
+        int age;
+        string egn;
+        DateTime birthDate;
+        
+
+
+        public string Name
+        {
+            get { return name; }
+            set {name = value; }
+        }
+
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+        
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+
+        public string Egn
+        {
+            get { return egn; }
+            set { egn = value; }
+        }
+
+        public string Gender { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name,int age,string egn,string sex)
+        {
+            this.age = age;
+            this.name = name;
+            this.egn = egn;
+            this.Gender = sex;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("Person name : {0} , Sex : {1}",name ,Gender);
+        }
+
+
+    }
+}
