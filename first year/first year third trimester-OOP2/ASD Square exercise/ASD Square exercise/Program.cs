b9006939d9508a4371a79333ce7688fed4154bb8
Velin Dimitrov <bgrullerbg@abv.bg>
2018-05-25 16:23:39 +0300

    Adding Square exercise

---
+++ first year/first year third trimester-OOP2/ASD Square exercise/ASD Square exercise/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASD_Square_exercise
+{
+    class Program
+    {
+        static List<List<Square>> groupedSquares = new List<List<Square>>();
+        static void Main(string[] args)
+        {
+            int numberOfSquares = int.Parse(Console.ReadLine());
+            List<Square> squares = new List<Square>();
+            for (int i = 0; i < numberOfSquares; i++)
+            {
+                int[] cordinates = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+                squares.Add(new Square(cordinates[0], cordinates[1], cordinates[2], cordinates[3]));
+            }
+
+            findGroupedSquares(squares);
+            double sum=0;
+            for (int i = 0; i < groupedSquares.Count; i++)
+            {
+                sum += CalculateGroupArea(groupedSquares[i]);
+                Console.WriteLine($"Area of group {i+1} :{CalculateGroupArea(groupedSquares[i])}");
+            }
+            Console.WriteLine("Sum of all groups : "+sum);
+            Console.WriteLine("Number of Groups : "+groupedSquares.Count);
+        }
+
+        private static void findGroupedSquares(List<Square> squares)
+        {
+            int currentIndex = 0;
+
+            for (int i = 1; i < squares.Count ; i++)
+            {
+                if (groupedSquares.Count<=currentIndex)
+                {
+                    groupedSquares.Add(new List<Square>());
+                }
+                
+
+                bool xCondition = squares[i-1].x2 >= squares[i].x1 + 1 && squares[i-1].y1 >= squares[i].y1 && squares[i-1].y1 <= squares[i].y2;
+                bool increasingYCondition = squares[i-1].x2 == squares[i].x1 && squares[i-1].y2 >= squares[i].y1 + 1 && squares[i-1].y2 <= squares[i].y2;
+                bool decreasingYCondition = squares[i-1].x2 == squares[i].x1 && squares[i].y2 <= squares[i-1].y2 && squares[i].y2 >= squares[i-1].y1+1;
+
+                if (xCondition || increasingYCondition||decreasingYCondition)
+                {                    
+                    groupedSquares[currentIndex].Add(squares[i-1]);                   
+                   
+                }
+                else
+                {
+                    if (i == 1)
+                    {
+                        groupedSquares[currentIndex].Add(squares[i - 1]);
+                        currentIndex++;
+                        continue;
+                    }
+                    currentIndex++;
+                    groupedSquares.Add(new List<Square>());
+                    groupedSquares[currentIndex].Add(squares[i-1]);                    
+                }
+
+                if (i==squares.Count-1&&(xCondition||decreasingYCondition||increasingYCondition))
+                {
+                    groupedSquares[currentIndex].Add(squares[i]);
+                }
+
+            }
+        }
+
+        public static double CalculateGroupArea(List<Square> group) {
+            double sum = 0;
+            foreach (var square in group)
+            {
+                sum += square.CalculateArea();
+            }
+            return sum;
+        }
+    }
+}
---
+++ first year/first year third trimester-OOP2/ASD Square exercise/ASD Square exercise/Square.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASD_Square_exercise
+{
+    class Square
+    {
+        private Point A;
+        private Point B;
+        private Point C;
+        private Point D;
+        private double sideA;
+        private double sideB;
+        public double x1;
+        public double x2;
+        public double y1;
+        public double y2;
+
+        public Square(double x1,double y1,double x2,double y2)
+        {
+            this.A = new Point(x1, y1);
+            this.C = new Point(x2, y2);
+            this.B = new Point(x2, y1);
+            this.D = new Point(x1, y2);
+            this.sideA=calculateSide(A,B);
+            this.sideB = calculateSide(A, D);
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;            
+        }
+        
+        private double calculateSide(Point a, Point b)
+        {
+            double firstPair = Math.Abs(b.x - a.x);
+            double secondPair = Math.Abs(b.y - a.y);
+            return Math.Sqrt(Math.Pow(firstPair,2)+Math.Pow(secondPair,2));
+        }
+
+        public double CalculateArea()
+        {
+            return sideA * sideB;
+        }
+        //static void Main(string[] args) { //test
+        //    Square sqr = new Square(2, 0, 4, 1);
+        //    sqr.CalculateArea();
+
+        //}
+    }
+}
