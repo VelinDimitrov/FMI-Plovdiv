3e3f5a95e0f0d90258df03ec64c766e4f5e03a3e
Velin Dimitrov <bgrullerbg@abv.bg>
2018-05-29 14:07:32 +0300

    Last fix on Squares

--- first year/first year third trimester-OOP2/ASD Square exercise/ASD Square exercise/Program.cs
+++ first year/first year third trimester-OOP2/ASD Square exercise/ASD Square exercise/Program.cs
@@ -18,8 +18,10 @@ namespace ASD_Square_exercise
                 int[] cordinates = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
                 squares.Add(new Square(cordinates[0], cordinates[1], cordinates[2], cordinates[3]));
             }
+
             SortSquares(squares);
             findGroupedSquares(squares);
+
             double sum=0;
             for (int i = 0; i < groupedSquares.Count; i++)
             {
@@ -49,41 +51,55 @@ namespace ASD_Square_exercise
         {
             int currentIndex = 0;
 
-            for (int i = 1; i < squares.Count ; i++)
-            {
-                if (groupedSquares.Count<=currentIndex)
+            for (int i = 0; i < squares.Count ; i++)
+            {                
+                if (i!=0)
                 {
-                    groupedSquares.Add(new List<Square>());
-                }
-                
-
-                bool xCondition = squares[i-1].x2 >= squares[i].x1 + 1 && squares[i-1].y1 >= squares[i].y1 && squares[i-1].y1 <= squares[i].y2;
-                bool increasingYCondition = squares[i-1].x2 == squares[i].x1 && squares[i-1].y2 >= squares[i].y1 + 1 && squares[i-1].y2 <= squares[i].y2;
-                bool decreasingYCondition = squares[i-1].x2 == squares[i].x1 && squares[i].y2 <= squares[i-1].y2 && squares[i].y2 >= squares[i-1].y1+1;
-
-                if (xCondition || increasingYCondition||decreasingYCondition)
-                {                    
-                    groupedSquares[currentIndex].Add(squares[i-1]);                   
-                   
+                    if (IsInGroup(squares,i))
+                    {
+                        groupedSquares[currentIndex].Add(squares[i]);
+                    }
+                    else
+                    {
+                        currentIndex++;
+                           groupedSquares.Add(new List<Square>());
+                           groupedSquares[currentIndex].Add(squares[i]);  
+                    }
                 }
                 else
                 {
-                    if (i == 1)
-                    {
-                        groupedSquares[currentIndex].Add(squares[i - 1]);                        
-                        continue;
-                    }
-                    currentIndex++;
                     groupedSquares.Add(new List<Square>());
-                    groupedSquares[currentIndex].Add(squares[i-1]);                    
-                }
-
-                if (i==squares.Count-1&&(xCondition||decreasingYCondition||increasingYCondition))
-                {
                     groupedSquares[currentIndex].Add(squares[i]);
-                }               
+                }                       
+                                    
 
             }
+        }
+
+        private static bool IsInGroup(List<Square> squares, int i)
+        {
+
+            bool positiveXCondition = squares[i - 1].x2 >= squares[i].x1 + 1 && squares[i - 1].y1 >= squares[i].y1 && squares[i - 1].y1 <= squares[i].y2;
+            bool increasingYCondition = squares[i - 1].x2 == squares[i].x1 && squares[i - 1].y2 >= squares[i].y1 + 1 && squares[i - 1].y2 <= squares[i].y2;
+            bool decreasingYCondition = squares[i - 1].x2 == squares[i].x1 && squares[i].y2 <= squares[i - 1].y2 && squares[i].y2 >= squares[i - 1].y1 + 1;
+
+            bool result = positiveXCondition || decreasingYCondition || increasingYCondition;
+            if (areBothBottom(squares, i, result))
+            {
+                bool negativeCondition = (squares[i - 1].x2 - squares[i].x1 >= 0) &&
+                    (squares[i - 1].y2 - squares[i].y2 >= 0 || squares[i - 1].y2 - squares[i].y2 <= 0);
+                result = negativeCondition;
+            }
+            
+            return result;
+        }
+        private static bool IsBottomAndTop(List<Square> squares, int i, bool result)
+        {
+            return !result && (squares[i - 1].y2 < 0 && squares[i].y2 > 0|| squares[i - 1].y2 > 0 && squares[i].y2 < 0);
+        }
+        private static bool areBothBottom(List<Square> squares, int i, bool result)
+        {
+            return !result && squares[i - 1].y2 <= 0 && squares[i].y2 <= 0;
         }
 
         public static double CalculateGroupArea(List<Square> group) {
