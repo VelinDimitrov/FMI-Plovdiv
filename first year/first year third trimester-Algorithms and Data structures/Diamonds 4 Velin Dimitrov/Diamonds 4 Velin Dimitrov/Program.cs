f4994d2983c7e85acbd3933d7ead5b31c16176a7
Velin Dimitrov <bgrullerbg@abv.bg>
2018-06-02 20:46:26 +0300

    Adding Diamonds exercise

---
+++ first year/first year third trimester-Algorithms and Data structures/Diamonds 4 Velin Dimitrov/Diamonds 4 Velin Dimitrov/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamonds_4_Velin_Dimitrov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter one side Diamonds :");
+            int dominoOneSideDiamonds = int.Parse(Console.ReadLine());
+            int totalDiamonds = (dominoOneSideDiamonds + 2) * (dominoOneSideDiamonds*(dominoOneSideDiamonds+1)/2);
+            Console.WriteLine("Total Diamonds :"+totalDiamonds);
+        }
+    }
+}
